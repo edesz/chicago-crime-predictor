5d849ddc36bbdb672703083f309db6aa2687023e
edesz <elsdes3@gmail.com>
2020-02-25 13:15:26 -0500

    Update tasks.sh

--- tasks.sh
+++ tasks.sh
@@ -8,8 +8,8 @@ if [[ $state == "build" ]]; then
     tox -e build
 elif [[ $state == "ci" ]]; then
     tox -e ci
-elif [[ $state == "dockerbuild" ]]; then
-    tox -e dockerbuild
+elif [[ $state == "dockerci" ]]; then
+    tox -e dockerci
 elif [[ $state == "view" ]]; then
     tox -e view
 elif [[ $state == "dockerview" ]]; then
